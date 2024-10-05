a021718916699dfd7412f84297de43a7a50957ca
El-Minster <El-Minster@MSI>
2024-10-05 16:41:30 +0200

    Test v2

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function checkForMatch() {
     });
 
     Object.keys(imageCount).forEach(image => {
-        if (imageCount[image].length === 2) {
+        if (imageCount[image].length === 8) {
             imageCount[image].forEach(item => {
                 item.style.backgroundColor = 'black';
 
